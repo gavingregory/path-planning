c8fb438a73a810b75ed79e9da89fb4510d120a03
Gavin Gregory <gavin.ian.gregory@gmail.com>
2016-03-21 20:05:37 +0000

    Initial commit: empty project set up with Irrlicht 1.8.3.

---
+++ path-core/src/main.cpp
@@ -0,0 +1,14 @@
+#include <irrlicht.h>
+#include <iostream>
+
+using namespace irr;
+
+#pragma comment(lib, "Irrlicht.lib")
+
+int main(void) {
+	
+	std::cout << "Whatever trevor" << std::endl;
+	int x;
+	std::cin >> x;
+	return 0;
+}
